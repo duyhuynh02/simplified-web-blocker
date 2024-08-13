009806d134ca5ce0a90e82d9c38150063d17b10b
Duy Huynh <minhduyhuynh16@gmail.com>
2024-08-13 21:06:02 +0700

    dynamically blocking

--- background.js
+++ background.js
@@ -1,7 +1,29 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        return { cancel: true };
-    }, 
-    {urls: ["*://www.x.com/*"]},
-    ["blocking"]
-)
+chrome.runtime.onInstalled.addListener(() => {
+
+    // Websites to block for testings 
+    const websitesToBlock = [
+        "test-website.com",
+        "another-test-website.com",
+    ]
+    
+    // An array for websites 
+    const dynamicWebsitesArray = websitesToBlock.map((site, index) => ({
+        id: index + 1, 
+        priority: 1, 
+        action: { type: "block" },
+        condition: {
+            urlFilter: site,
+            resourceTypes: ["main_frame"]
+        }
+    })); 
+
+    console.log('hello: ', dynamicWebsitesArray);
+
+    //main 
+    chrome.declarativeNetRequest.updateDynamicRules({
+        removeRuleIds: [1, 2],
+        addRules: dynamicWebsitesArray
+    }, () => {
+        console.log("Dynamic blocking rules have been added.")
+    })
+})
--- todo.md
+++ todo.md
@@ -3,7 +3,7 @@
 - [x] Preparation for html popup
 - [x] button id = addSite; id for each ul -> siteList 
 - [x] Complete popup.js -> get all the blocked websites 
-- [] Complete background.js -> block website function
+- [] Complete background.js -> block website function 
 
 
 
