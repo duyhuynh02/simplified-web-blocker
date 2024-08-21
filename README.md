818fabf33a0ca17bccd2e8951c528111708ee0a4
Duy Huynh <minhduyhuynh16@gmail.com>
2024-08-21 16:31:05 +0700

    done the blocked sites with hours + add readme

---
+++ README.md
@@ -0,0 +1,46 @@
+# Simplified Website Blocker
+This Chrome extension allows users to block specific websites and set a countdown timer to automatically unblock them after a specified period. It provides options for blocking websites for 30 minutes, 60 minutes, or indefinitely ("unlimited" time).
+
+## Features
+- Website Blocking: Users can block specific websites, preventing access to them in the browser.
+- Countdown Timer: Users can set a timer (30 minutes, 60 minutes, or unlimited) after which the blocked sites will be automatically unblocked.
+
+## Installation
+1. Clone or download the repo:
+```bash
+
+```
+
+2. Load the Extension in Chrome:
+- Open Chrome and navigate to chrome://extensions/.
+- Enable "Developer mode" in the top-right corner.
+- Click "Load unpacked" and select the project directory.
+
+3. Use the Extension:
+- Click the extension icon in the Chrome toolbar.
+- Add websites to the block list by entering the URL and clicking "Add Site."
+- Select a countdown timer option and click "Set Timer."
+- Manage the block list and timer settings as needed.
+
+## Contributing
+If you have suggestions or want to contribute to the project, feel free to fork the repository and submit a pull request.
+
+## License
+This project is licensed under the MIT License.
+
+
+# Tasks list
+- [x] Set up manifest, popup, icon and background.js file 
+- [x] Preparation for html popup
+- [x] button id = addSite; id for each ul -> siteList 
+- [x] Complete popup.js -> get all the blocked websites 
+- [x] Complete background.js -> block website function 
+- [x] Show list of blocked websites 
+- [x] Remove blocked websites 
+- [x] Add a time (30 - 60) for leisure time 
+- [x] Clear existing after add new site 
+- [x] Show the new blocked website immediately 
+- [x] Block websites during certain time 
+
+
+
--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 // Initialize time 
-const defaultValue = "unlimited"
-let selectedValue = defaultValue 
+const ONE_MINUTE_MS = 60000;
+const defaultValue = "unlimited";
+let selectedValue = defaultValue;
 
 // Initialization
 document.addEventListener('DOMContentLoaded', () => {
@@ -12,47 +13,32 @@ document.addEventListener('DOMContentLoaded', () => {
             if (result.countDownDate) {
                 let remainingTime = result.countDownDate - new Date().getTime(); 
                 if (remainingTime > 0) {
-                    countDownTime(remainingTime / (60 * 1000));
+                    countDownTime(remainingTime / ONE_MINUTE_MS);
                 } else {
-                    document.getElementById("countdown").innerHTML = 'Time is up'; 
+                    clearBlockedSites();
+                    document.getElementById("countdown").innerHTML = 'Time is up';
                 }
             } else {
                 document.getElementById("countdown").innerHTML = "Unlimited time";
             }
 
             // Loading all the blocked sites for the first initialization
-            const websitesContainer = document.getElementById('website-container');
             let blockedSites = result.blockedSites || []; 
             blockedSites.forEach((site, index) => {
-                // Create li element 
-                let li = document.createElement('li');
-                li.textContent = site; 
-
-                // Create delete element 
-                let deleteButton = document.createElement('button');
-                deleteButton.textContent = "Delete"; 
-
-                // Create div element and add two li and delete elements into it 
-                let div = document.createElement('div');
-                div.className = 'list-website-div';
-                div.appendChild(li);
-                div.appendChild(deleteButton);
-
-                // Finally add to website-container for each element 
-                websitesContainer.appendChild(div);
+                addSideToUI(blockedSites);
             })
         }
     });
 })
 
-// main 
+// main to add a site 
 document.getElementById('addSite').addEventListener('click', () => { 
     let siteInput = document.getElementById('siteInput');
     // To clear the existing data later; 
-    let site = siteInput.value;
+    let site = siteInput.value.trim();
 
     // whitespace is not approriate 
-    if (site.trim()) {
+    if (site) {
         chrome.storage.sync.get(['blockedSites'], (result) => {
             let blockedSites = result.blockedSites || []; 
             blockedSites.push(site); 
@@ -64,32 +50,14 @@ document.getElementById('addSite').addEventListener('click', () => {
                 } else {
                     // update the blocking rule 
                     updateBlockingRules(blockedSites);
-    
-                    // update to show immediately 
-                    const websitesContainer = document.getElementById('website-container');
-                    // Create li element 
-                    let li = document.createElement('li');
-                    li.textContent = site; 
-
-                    // Create delete element 
-                    let deleteButton = document.createElement('button');
-                    deleteButton.textContent = "Delete"; 
-
-                    // Create div element and add two li and delete elements into it 
-                    let div = document.createElement('div');
-                    div.className = 'list-website-div';
-                    div.appendChild(li);
-                    div.appendChild(deleteButton);
-
-                    // Finally add to website-container for each element 
-                    websitesContainer.appendChild(div);
+                    addSideToUI(site);
                 }
             });
         });
     } 
 });
 
-// Select button
+// Handle selection of time options
 document.getElementById('selectedOptionBtn').addEventListener('click', () => {
     let form = document.getElementById('optionsForm'); 
     let selectedOption = form.querySelector('input[name="option"]:checked');
@@ -100,16 +68,17 @@ document.getElementById('selectedOptionBtn').addEventListener('click', () => {
         document.getElementById('selectedOption').innerText = `You selected: ${selectedValue} minutes`; 
         document.getElementById("countdown").innerHTML = '';
         // refresh to make sure that ok 
-        window.location.reload();
+        // window.location.reload();           // not working due to this 
         if (selectedValue !== "unlimited") {
             let timeInMinutes = Number(selectedValue)
-            let countDownDate = new Date().getTime() + timeInMinutes * 60 * 1000;
+            let countDownDate = new Date().getTime() + timeInMinutes * ONE_MINUTE_MS;
 
             // save the countdown date end time 
             chrome.storage.sync.set({ countDownDate: countDownDate }, () => {
                 if (chrome.runtime.lastError) {
                     console.error("Failed to save countdown date:", chrome.runtime.lastError);
                 } else {
+                    // countDownTime(0.3);     //testing purspose
                     countDownTime(timeInMinutes);
                 }
             })
@@ -125,7 +94,7 @@ document.getElementById('selectedOptionBtn').addEventListener('click', () => {
     }
 })
 
-// Remove function 
+// Remove blocked sites  
 document.getElementById('website-container').addEventListener('click', (event) => {
     console.log('Click detected:', event.target);
     if (event.target.tagName === 'BUTTON') {    //must be uppercase for tagName 
@@ -155,7 +124,7 @@ document.getElementById('website-container').addEventListener('click', (event) =
 })
 
 
-function updateBlockingRules(sites) {
+function updateBlockingRules(sites) { //default to unlimited time 
     // An array for websites 
     const dynamicWebsitesArray = sites.map((site, index) => ({
         id: index + 1, 
@@ -172,24 +141,63 @@ function updateBlockingRules(sites) {
         addRules: dynamicWebsitesArray
     }, () => {
         console.log("Dynamic blocking rules have been added.")
-    })
+    });
+
 }
 
 function countDownTime(time) {      // time is in minute 
     // Only 3 options: 30 or 60 or unlimited time 
-    let countDownDate = new Date().getTime() + time * 60 * 1000; //change to milliseconds
+    let countDownDate = new Date().getTime() + time * ONE_MINUTE_MS; //change to milliseconds
     let x = setInterval(() => {
         let now = new Date().getTime(); 
         let distance = countDownDate - now;
-        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)); 
-        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        let minutes = Math.floor((distance % (ONE_MINUTE_MS * 60)) / (ONE_MINUTE_MS)); 
+        let seconds = Math.floor((distance % (ONE_MINUTE_MS)) / 1000);
         document.getElementById("countdown").innerHTML = `${minutes} m ${seconds} s`;
 
         // Countdown is done, show to users 
         if (distance < 0) {
             clearInterval(x);
+            clearBlockedSites();
+            clearTime();
             document.getElementById("countdown").innerHTML = 'Time is up!';
         }
 
     }, 1000);
+}
+
+function addSideToUI(site) {
+    const websitesContainer = document.getElementById('website-container');
+
+    let li = document.createElement('li');
+    li.textContent = site; 
+
+    // Create delete element 
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = "Delete"; 
+
+    // Create div element and add two li and delete elements into it 
+    let div = document.createElement('div');
+    div.className = 'list-website-div';
+    div.appendChild(li);
+    div.appendChild(deleteButton);
+
+    // Finally add to website-container for each element 
+    websitesContainer.appendChild(div);
+}
+
+// Clear blocked site after time is up; 
+function clearBlockedSites() {
+    chrome.storage.sync.set({ blockedSites: [] }, () => {
+        updateBlockingRules([]);
+        document.getElementById('website-container').innerHTML = ``;
+        console.log("Blocked sites have been cleared.");
+    });
+}
+
+// Clear time after time is up also;
+function clearTime() {
+    chrome.storage.sync.set({ countDownTime: "unlimited" }, () => {
+        console.log("Time have been back to the initial stage.")
+    })
 }
